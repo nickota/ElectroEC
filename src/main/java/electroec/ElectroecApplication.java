2f840c7f9fa2118882b02636cbdbf5292b8360ad
nickota <xxk0ta0605@gmail.com>
2019-06-15 15:26:34 +0900

    First Commit

---
+++ src/main/java/electroec/ElectroecApplication.java
@@ -0,0 +1,13 @@
+package electroec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectroecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectroecApplication.class, args);
+	}
+
+}
