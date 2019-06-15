49207d377f84941cb5bfe4c520a7afdd08864af8
nickota <xxk0ta0605@gmail.com>
2019-06-15 17:56:07 +0900

    Create controller.

---
+++ src/main/java/electroec/controller/ElectroEcController.java
@@ -0,0 +1,19 @@
+package electroec.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class ElectroEcController {
+
+	@RequestMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping("/")
+	public String home(Model model) {
+		return "redirect:index";
+	}
+}
