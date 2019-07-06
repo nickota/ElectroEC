0237aa24695b5430265340132a1b1b8bebe4fad8
nickota <xxk0ta0605@gmail.com>
2019-07-06 12:55:43 +0900

    Trying to connect to mysql

--- src/main/java/com/example/electroec/controller/ElectroEcController.java
+++ src/main/java/com/example/electroec/controller/ElectroEcController.java
@@ -1,24 +1,38 @@
 package com.example.electroec.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.electroec.entity.Products;
+import com.example.electroec.repository.ProductRepository;
 
 @Controller
 public class ElectroEcController {
 
-	@RequestMapping("/index")
-	public String index() {
-		return "index";
-	}
+	@Autowired
+	private ProductRepository productRepository;
 
-	@RequestMapping("/store")
-	public String store() {
-		return "store";
+	@RequestMapping("/index")
+	public List<Products> get() {
+		return productRepository.findAll();
 	}
-	
-	@RequestMapping("/")
-	public String home(Model model) {
-		return "redirect:index";
-	}
+	// public String index() {
+//		return "index";
+//	}
+
+//	@RequestMapping("/store")
+//	public List<Products> get() {
+//		return productRepository.findAll();
+//	}
+	// public String store() {
+//		return "store";
+//	}
+
+//	@RequestMapping("/")
+//	public String home(Model model) {
+//		return "redirect:index";
+//	}
 }
