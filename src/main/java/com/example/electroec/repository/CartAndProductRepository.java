0f63f2d819b5926a8c5e59aa4991d1126ab314c5
nickota <xxk0ta0605@gmail.com>
2019-08-09 19:23:46 +0900

    Create CartAndProductRepository.

---
+++ src/main/java/com/example/electroec/repository/CartAndProductRepository.java
@@ -0,0 +1,7 @@
+package com.example.electroec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CartAndProductRepository extends JpaRepository<T, ID> {
+
+}
