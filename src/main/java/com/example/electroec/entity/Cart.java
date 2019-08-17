414e5be7df9f0da007fc67cc641ed81dc49548c2
nickota <xxk0ta0605@gmail.com>
2019-08-17 17:00:42 +0900

    Add GenerationType.IDENTITY to avoid errors when inserting.

--- src/main/java/com/example/electroec/entity/Cart.java
+++ src/main/java/com/example/electroec/entity/Cart.java
@@ -6,6 +6,7 @@ import java.util.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -30,7 +31,7 @@ public class Cart implements Serializable {
 
 	@Column(name = "id")
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	@Column(name = "customer_id")
