3bfa6bbbeedf3eb841f27ffbd4eb028eef1b1d28
nickota <xxk0ta0605@gmail.com>
2019-09-07 16:00:28 +0900

    Add @OneToOne to Cart and Customer entity.

--- src/main/java/com/example/electroec/entity/Customer.java
+++ src/main/java/com/example/electroec/entity/Customer.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -89,6 +90,9 @@ public class Customer implements Serializable {
 	@Column(name = "update_user")
 	@NotNull
 	private String updateUser;
+
+	@OneToOne
+	private Cart cart;
 
 	/**
 	 * {@inheritDoc}
