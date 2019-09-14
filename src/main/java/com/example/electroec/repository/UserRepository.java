1adaf781817aa0c65ce96f4669d24de8df458cf4
nickota <xxk0ta0605@gmail.com>
2019-09-14 11:32:36 +0900

    Create user service and repository.

---
+++ src/main/java/com/example/electroec/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.electroec.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.electroec.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+	public User findOne(Integer id);
+}
---
+++ src/main/java/com/example/electroec/service/UserService.java
@@ -0,0 +1,18 @@
+package com.example.electroec.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.electroec.entity.User;
+import com.example.electroec.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	UserRepository userRepository;
+
+	public User findOne(Integer id) {
+		return userRepository.findOne(id);
+	}
+}
