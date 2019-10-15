ce42cc3304f2be8623c6033e7b1aba6f4b22344f
nickota <xxk0ta0605@gmail.com>
2019-10-15 21:44:23 +0900

    CartItems Entity.

--- src/main/java/com/example/electroec/entity/CartItems.java
+++ src/main/java/com/example/electroec/entity/CartItems.java
@@ -8,6 +8,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
 import lombok.Data;
@@ -20,6 +21,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "cartitems")
 public class CartItems implements Serializable {
 
 	/** serialVersionUID. */
