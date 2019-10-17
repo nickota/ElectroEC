aee16ff5ad6bd2a77d361ea919294fa181b450fc
nickota <xxk0ta0605@gmail.com>
2019-10-17 22:44:46 +0900

    Input CartDTO fields.

--- src/main/java/com/example/electroec/dto/CartDTO.java
+++ src/main/java/com/example/electroec/dto/CartDTO.java
@@ -1,5 +1,13 @@
 package com.example.electroec.dto;
 
+import com.example.electroec.entity.Product;
+
+import lombok.Data;
+
+@Data
 public class CartDTO {
 
+	private Integer userId;
+	private Product product;
+	private Integer quantity;
 }
